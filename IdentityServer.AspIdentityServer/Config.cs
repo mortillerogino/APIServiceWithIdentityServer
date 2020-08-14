84ab5713cb3301f431667d7a947664ad6367eedd
Gino Mortillero <mortillerogino@gmail.com>
2020-08-14 18:59:41 +0800

    Removed more unnecessary code

--- IdentityServer.AspIdentityServer/Config.cs
+++ IdentityServer.AspIdentityServer/Config.cs
@@ -26,39 +26,6 @@ namespace IdentityServer.AspIdentityServer
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
-            // machine to machine client
-                new Client
-                {
-                    ClientId = "client",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    // scopes that client has access to
-                    AllowedScopes = { "api1" }
-                },
-
-                // interactive ASP.NET Core MVC client
-                new Client
-                {
-                    ClientId = "mvc",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    // where to redirect to after login
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
-
-                    // where to redirect to after logout
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
-
-                    AllowedScopes = new List<string>
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    }
-                },
-
                 new Client
                 {
                     ClientId = "postman",
