9a06f21763655b6ca315d5dc4778d264b714f012
Gino Mortillero <mortillerogino@gmail.com>
2020-08-26 18:08:03 +0800

    Fixed dlls required for migration. Added migration to add property ReferrerId

---
+++ IdentityServer.Data/Migrations/20200826100633_Add-Column-ReferrerId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Data.Migrations
+{
+    public partial class AddColumnReferrerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferrerId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferrerId",
+                table: "AspNetUsers");
+        }
+    }
+}
--- IdentityServer.Data/Models/ApplicationUser.cs
+++ IdentityServer.Data/Models/ApplicationUser.cs
@@ -5,5 +5,6 @@ namespace IdentityServer.Data.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string ReferrerId { get; set; }
     }
 }
